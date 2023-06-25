e56e52bd3af2fb1f39041416151d05a11af57ec3
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2023-06-25 08:41:57 +0700

    user data generator

--- readme.md
+++ readme.md
@@ -8,6 +8,10 @@ Untuk menggunakannya lakukan instalasi node.js terlebih dahulu. Setelah berhasil
 
 `npm install`
 
-untuk menajalankan program nya, gunakan comand:
+untuk menajalankan program nya, gunakan comand (SMART DOOR):
 
 `npm run generate`
+
+untuk menajalankan program nya, gunakan comand (UNIVERSAL):
+
+`npm run usergenerator`
---
+++ userGenerator.js
@@ -0,0 +1,145 @@
+import inquirer from "inquirer";
+import figlet from "figlet";
+import chalk from "chalk";
+import { createSpinner } from "nanospinner";
+import fs from "fs";
+import { faker } from "@faker-js/faker";
+
+let payload = [];
+let dataSize;
+let fixSize;
+let dataName;
+let dataType;
+let isDuplicteDataAllow;
+
+async function dataSizeSetter() {
+    const answer = await inquirer.prompt({
+        name: "data_size",
+        type: "input",
+        message: chalk.green("How mach data you want generate: "),
+    });
+    dataSize = answer.data_size;
+    fixSize = dataSize;
+    if (Number.isNaN(Number(dataSize))) {
+        console.log(
+            chalk.bold.red("Operation failed, please enter correct number")
+        );
+        dataSize = 0;
+        return;
+    }
+}
+
+async function dataTypeSetter() {
+    const answer = await inquirer.prompt({
+        name: "data_type",
+        type: "list",
+        message: chalk.green("What kind of data do you want to create:"),
+        choices: ["User Data"],
+    });
+    dataType = answer.data_type;
+}
+
+async function fileNameSetter() {
+    const answer = await inquirer.prompt({
+        name: "data_name",
+        type: "input",
+        message: chalk.green("The name for the file you created: "),
+    });
+    dataName = answer.data_name + ".json";
+
+    fs.access(`./${dataName}`, fs.F_OK, (err) => {
+        if (!err) {
+            console.error(err);
+            dataName =
+                answer.data_name +
+                String(new Date(Date.now()))
+                    .replaceAll(" ", "")
+                    .slice(0, 20)
+                    .replaceAll(":", "") +
+                ".json";
+            console.log(
+                chalk.bold.yellow(
+                    `File name already exist, file rename to "${dataName}"`
+                )
+            );
+            return;
+        }
+    });
+}
+
+async function duplicateDataSetter() {
+    const answer = await inquirer.prompt({
+        name: "is_allow",
+        type: "confirm",
+        message: "Is duplicate data allowed",
+    });
+    isDuplicteDataAllow = answer.is_allow;
+}
+
+console.log(
+    chalk.grey(
+        figlet.textSync("Data Generator", {
+            font: "ogre",
+            horizontalLayout: "default",
+            verticalLayout: "default",
+            width: 80,
+            whitespaceBreak: true,
+        })
+    )
+);
+
+await dataSizeSetter();
+await dataTypeSetter();
+await duplicateDataSetter();
+await fileNameSetter();
+const spinner = createSpinner();
+
+switch (dataType) {
+    case "User Data":
+        while (dataSize > 0) {
+            const data = {
+                username: faker.internet.userName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(8) + "@1Az",
+            };
+            if (payload.length > 0 && !isDuplicteDataAllow) {
+                const duplicatedata = payload.find((storeData) => {
+                    if (
+                        storeData.email === data ||
+                        storeData.username === data.username
+                    )
+                        return true;
+                });
+
+                if (duplicatedata) {
+                    dataSize++;
+                } else {
+                    payload.push(data);
+                }
+            } else {
+                payload.push(data);
+            }
+
+            spinner.start({
+                text: chalk.greenBright(
+                    `Generating data, please wait. Remain Queue ${dataSize}/${fixSize}`
+                ),
+            });
+            dataSize--;
+        }
+        break;
+
+    default:
+        break;
+}
+
+fs.writeFile(dataName, `${JSON.stringify(payload)}`, function (err) {
+    if (err) return 1;
+    console.log("File is created successfully.");
+});
+spinner.success({
+    text: chalk.greenBright(
+        "Success genereting data, file succesfully save with name " + dataName
+    ),
+    color: "green",
+});
